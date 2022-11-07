f6b4fa2ad7bb739e095c0eee11816130a0da00fe
Lorena Góes <goesloren@gmail.com>
2022-11-06 23:54:27 -0300

    init project with docker

---
+++ app/database/script.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS easywallet;
+USE easywallet;
---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
