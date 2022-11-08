f9a79442cb159295625d673282ae2e19e992ee7b
Lorena Góes <goesloren@gmail.com>
2022-11-07 22:05:36 -0300

    start containers with docker compose

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+require('dotenv').config();
+
+require('../database/config');
+
 const app = express();
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT
 
 app.get('/', (_req, res) => res.send('Hello World!'));
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 });
